98ddcf6be215d5f7fdd495d230bf1e19eda75499
benwiz <bwisialowski@gmail.com>
2019-10-02 10:22:55 -0500

    Use OpenID Connect

--- src/blaze/middleware/authentication.clj
+++ src/blaze/middleware/authentication.clj
@@ -1,6 +1,10 @@
 (ns blaze.middleware.authentication
+  "Ring middleware that verifies a signed JWT using OpenID Connect
+   to provide the public key used to sign the token. The public key
+   is stored in an atom that is updated every `expiration-minutes`."
   (:require
    [clojure.string :as string]
+   [buddy.core.keys :as keys]
    [buddy.sign.jwt :as jwt]
    [cheshire.core :as json]
    [ring.util.response :as ring]
@@ -10,31 +14,29 @@
    (java.security KeyFactory)
    (java.security.spec X509EncodedKeySpec)))
 
-;; https://auth.breezeehr.com/auth/realms/patient-portal/.well-known/openid-configuration
+
+(def ^:private expiration-minutes 60)
 
 
-(def ^:private expiration-minutes 1)
-
-
-;; TODO: Acutally use proper OpenID methods
-(defn- get-public-key [url]
+(defn- public-key [url]
   (-> url
       slurp
       json/parse-string
-      (get "issuer")
+      (get "jwks_uri")
       slurp
-      json/parse-string
-      (get "public_key")))
+      (json/parse-string keyword)
+      :keys ;; Buddy expects keywords, not strings
+      first
+      keys/jwk->public-key))
 
 
-;; TODO: Figure out if I can do this with only one argument
 (defn public-key-atom-value [v]
-  {:public-key (get-public-key (:openid-url v))
+  {:public-key (public-key (:openid-url v))
    :timestamp  (.getTime (Date.))
    :openid-url (:openid-url v)})
 
 
-(defn- public-key-str
+(defn- updated-public-key-atom
   "When the public key atom is more than `expiration-minutes` old,
    update the public key. Always return the public key."
   [public-key-atom]
@@ -42,27 +44,18 @@
     (when (< (* expiration-minutes 60000)
              (- (.getTime (Date.)) (:timestamp @public-key-atom)))
       (swap! public-key-atom public-key-atom-value))
-    (:public-key @public-key-atom)))
-
-
-(defn- str->public-key [s]
-  (->> s
-       #^bytes .getBytes
-       (.decode (Base64/getDecoder))
-       X509EncodedKeySpec.
-       (.generatePublic (KeyFactory/getInstance "RSA"))))
+    public-key-atom))
 
 
 (defn- unsigned-token [public-key-atom signed-token]
   (try
-    (let [public-key (str->public-key (public-key-str public-key-atom))]
+    (let [public-key (:public-key @(updated-public-key-atom public-key-atom))]
       (jwt/unsign signed-token public-key {:alg :rs256}))
     (catch Exception e
-      ;; Return nil to signal failure to unsign token
       (:cause (Throwable->map e)))))
 
 
-(defn unauthenticated-response [message]
+(defn- unauthenticated-response [message]
   (-> (ring/response {:message message})
       (ring/status 403)))
 
@@ -78,8 +71,8 @@
         (let [auth-header (get-in request [:headers "authorization"])]
           (if (string/blank? auth-header) ;; If auth header is nil or empty, inform client
             (unauthenticated-response "Missing authorization header")
-            (let [bearer-token    (string/replace auth-header "Bearer " "")
-                  token           (unsigned-token public-key-atom bearer-token)]
+            (let [bearer-token (string/replace auth-header "Bearer " "")
+                  token        (unsigned-token public-key-atom bearer-token)]
               (if (string? token) ;; If token is a string, it really is an error message, pass along to client (TODO: Is this too much info to expose?)
                 (unauthenticated-response token)
                 (handler request)))))))))
