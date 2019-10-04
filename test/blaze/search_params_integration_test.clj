7df5f36b8c526736124fc8ae4fa536d65474bdda
drew verlee <dverlee@breezeehr.com>
2019-10-04 13:56:09 -0500

    Test seems to be loading patient with condition.

--- test/blaze/search_params_integration_test.clj
+++ test/blaze/search_params_integration_test.clj
@@ -5,18 +5,50 @@
    [clojure.test :refer :all]
    [datomic.api :as d]
    [datomic-spec.test :as dst]
-   [juxt.iota :refer [given]]
    [blaze.bundle :as bundle]
    [blaze.cql-translator :as cql]
    [blaze.datomic.test-util :as test-util]
    [blaze.elm.compiler :as compiler]
-   [blaze.elm.date-time :as date-time]
-   [blaze.elm.deps-infer :refer [infer-library-deps]]
-   [blaze.elm.equiv-relationships :refer [find-equiv-rels-library]]
-   [blaze.elm.normalizer :refer [normalize-library]]
    [blaze.elm.spec]
-   [blaze.elm.type-infer :refer [infer-library-types]]
    [blaze.elm.evaluator :as evaluator]
    [blaze.terminology-service.extern :as ts]
-   [taoensso.timbre :as log]))
+   [taoensso.timbre :as log])
+  (:import
+   [java.time OffsetDateTime Year]))
+
+
+(defonce db (d/db (st/with-instrument-disabled (test-util/connect))))
+
+
+(defn fixture [f]
+  (st/instrument)
+  (dst/instrument)
+  (log/with-merged-config {:level :error} (f))
+  (st/unstrument))
+
+(use-fixtures :each fixture)
+
+(def term-service
+  (ts/term-service "http://tx.fhir.org/r4" {} nil nil))
+
+(defn db-with [{:strs [entries]}]
+  (let [entries        @(bundle/annotate-codes term-service db entries)
+        {db :db-after} (d/with db (bundle/code-tx-data db entries))]
+    (:db-after (d/with db (bundle/tx-data db entries)))))
+
+(defn- evaluate [db query]
+  @(evaluator/evaluate db (OffsetDateTime/now)
+                       (compiler/compile-library db (cql/translate query) {})))
+
+(defn read-data [query-name]
+  (-> (slurp (str "integration-test/" query-name "/data.json"))
+      (json/parse-string)))
+
+(def patient-with-condition (read-data "query-3"))
+
+(deftest sandbox
+  (let [db    (db-with patient-with-condition)
+        ;;TODO query to get data. Need to look into schema
+        query '[]]
+    (println (d/q query db))))
 
