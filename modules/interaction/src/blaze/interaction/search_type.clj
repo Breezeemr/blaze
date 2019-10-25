5868bb7198f528344d7786b9e2c32303b037262e
drew verlee <dverlee@breezeehr.com>
2019-10-25 10:09:46 -0500

    add subset fhir search param capabilities
    1. these are the search params that cabotage currently needs
    2. passes existing project tests but hasn't been run against any search param
    specific tests.
    3. I believe these accessible by clients via
    POST  [base]/[type]/_search{?[parameters]{&_format=[mime-type]}}

--- modules/interaction/src/blaze/interaction/search_type.clj
+++ modules/interaction/src/blaze/interaction/search_type.clj
@@ -3,40 +3,143 @@
 
   https://www.hl7.org/fhir/http.html#search"
   (:require
-    [blaze.datomic.pull :as pull]
-    [blaze.datomic.util :as util]
-    [blaze.handler.fhir.util :as fhir-util]
-    [blaze.middleware.fhir.metrics :refer [wrap-observe-request-duration]]
-    [clojure.spec.alpha :as s]
-    [datomic.api :as d]
-    [datomic-spec.core :as ds]
-    [integrant.core :as ig]
-    [reitit.core :as reitit]
-    [ring.middleware.params :refer [wrap-params]]
-    [ring.util.response :as ring]
-    [taoensso.timbre :as log]))
+   [blaze.datomic.pull :as pull]
+   [blaze.datomic.util :as util]
+   [blaze.handler.fhir.util :as fhir-util]
+   [blaze.middleware.fhir.metrics :refer [wrap-observe-request-duration]]
+   [clojure.spec.alpha :as s]
+   [datomic.api :as d]
+   [datomic-spec.core :as ds]
+   [integrant.core :as ig]
+   [reitit.core :as reitit]
+   [ring.middleware.params :refer [wrap-params]]
+   [ring.util.response :as ring]
+   [taoensso.timbre :as log]))
+
+(defn coding->code
+  [coding]
+  (-> coding
+      :Coding/code
+      :code/code))
+
+(defn codeable-concept->coding
+  [codeable-concept]
+  (map coding->code
+       (:CodeableConcept/coding codeable-concept)))
+
+(defn match-codeable-concept?
+  [search codeable-concept]
+  (contains? (set (codeable-concept->coding codeable-concept))
+             search))
+
+;;NOTE function assumes reference is always a patient.
+(defn match-reference?
+  [search reference]
+  (= (:Patient/id reference)
+     search))
 
 
-;; TODO: improve quick hack
-(defn- resource-pred [db type {:strs [identifier]}]
-  (when identifier
-    (let [attr (keyword type "identifier")
-          {:db/keys [cardinality]} (util/cached-entity db attr)
-          matches?
-          (fn [{:Identifier/keys [value]}]
-            (= identifier value))]
+(defn match-identifier?
+  [search identifier]
+  (= (:Identifier/value identifier)
+     search))
+
+(defn provision->actor-ids
+  [provision]
+  (map
+    (fn [m] (-> m :Consent.provision.actor/reference :Patient/id))
+    (:Consent.provision/actor provision)))
+
+(defn match-actor?
+  [search provision]
+  (contains? (set (provision->actor-ids provision))
+             search))
+
+(def res-type+search->matches-fn
+  {"Condition"          {"subject"  {:matches-fn match-reference?
+                                     :attr       :Condition/subject}
+                         "patient"  {:matches-fn match-reference?
+                                     :attr       :Condition/subject}
+                         "category" {:matches-fn match-codeable-concept?
+                                     :attr       :Condition/category}}
+   "MedicationRequest"  {"subject" {:matches-fn match-reference?
+                                    :attr       :MedicationRequest/subject}
+                         "patient" {:matches-fn match-reference?
+                                    :attr       :MedicationRequest/subject}}
+   "ServiceRequest"     {"subject" {:matches-fn match-reference?
+                                    :attr       :ServiceRequest/subject}
+                         "patient" {:matches-fn match-reference?
+                                    :attr       :ServiceRequest/subject}}
+   "Goal"               {"subject" {:matches-fn match-reference?
+                                    :attr       :Goal/subject}
+                         "patient" {:matches-fn match-reference?
+                                    :attr       :Goal/subject}}
+   "Procedure"          {"subject" {:matches-fn match-reference?
+                                    :attr       :Procedure/subject}
+                         "patient" {:matches-fn match-reference?
+                                    :attr       :Procedure/subject}}
+   "AllergyIntolerance" {"patient" {:matches-fn match-reference?
+                                    :attr       :AllergyIntolerance/patient}}
+   "Device"             {"patient" {:matches-fn match-reference?
+                                    :attr       :Device/patient}}
+   "Immunization"       {"patient" {:matches-fn match-reference?
+                                    :attr       :Immunization/patient}}
+   "Observation"        {"patient"  {:matches-fn match-reference?
+                                     :attr       :Observation/subject}
+                         "category" {:matches-fn match-codeable-concept?
+                                     :attr       :Observation/category}}
+   "DiagnosticReport"   {"subject" {:matches-fn match-reference?
+                                    :attr       :DiagnosticReport/subject}
+                         "patient" {:matches-fn match-reference?
+                                    :attr       :DiagnosticReport/subject}}
+   "CarePlan"           {"subject" {:matches-fn match-reference?
+                                    :attr       :CarePlan/subject}
+                         "patient" {:matches-fn match-reference?
+                                    :attr       :CarePlan/subject}}
+   ;; An "actor" is the entity with some action
+   ;; over a patients information
+   ;;TODO will probably need to check "action"
+   "Consent"            {"patient" {:matches-fn match-reference?
+                                    :attr       :Consent/patient}
+                         "actor"   {:matches-fn match-actor?
+                                    :attr       :Consent/provision}}
+   "identifier"         match-identifier?})
+
+(defn- resource-pred
+  [db type query-params]
+  (let [search-info (reduce-kv
+                      (fn [coll search-param search-value]
+                        (if-let [matches-fn (get-in res-type+search->matches-fn [type search-param :matches-fn])]
+                          (let [attr (get-in res-type+search->matches-fn [type search-param :attr])]
+                            (conj coll
+                                  {:search-param search-param
+                                   :search-value search-value
+                                   :matches-fn   matches-fn
+                                   :attr         attr
+                                   :cardinality  (:db/cardinality (util/cached-entity db attr))}))
+                          coll))
+                      []
+                      query-params)]
+    ;; NOTE this is removing invalid query but not rejecting the query
+    ;; if it has invalid params
+    (when (seq search-info)
       (fn [resource]
-        (let [value (get resource attr)]
-          (if (= :db.cardinality/many cardinality)
-            (some matches? value)
-            (matches? value)))))))
+        (every? true? (reduce
+                        (fn [matches {:keys [matches-fn attr search-value cardinality]}]
+                          (let [attr-instance (get resource attr)]
+                            (conj matches
+                                  (if (= :db.cardinality/many cardinality)
+                                    (some (partial matches-fn search-value) attr-instance)
+                                    (matches-fn search-value attr-instance)))))
+                        []
+                        search-info))))))
 
 
 (defn- entry
   [router {type "resourceType" id "id" :as resource}]
-  {:fullUrl (fhir-util/instance-url router type id)
+  {:fullUrl  (fhir-util/instance-url router type id)
    :resource resource
-   :search {:mode "match"}})
+   :search   {:mode "match"}})
 
 
 (defn- summary?
