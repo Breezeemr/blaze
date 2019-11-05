59e17e36717d5b4cc722b4874e6f238b3a451472
drew verlee <dverlee@breezeehr.com>
2019-11-05 14:31:39 -0600

    update emacs config file to just run dev in development mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,4 @@
   ;; select deps as the build tool
   (cider-preferred-build-tool . clojure-cli)
   ;; include dev dependencies when running
-  (cider-clojure-cli-global-options . "-A:dev:test")))
+  (cider-clojure-cli-global-options . "-A:dev")))
