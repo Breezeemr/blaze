9ab0a5052ccf6a60d3e0d8f9292ebe75e7feade5
drew verlee <dverlee@breezeehr.com>
2019-11-06 14:17:01 -0600

    use working dev namespace
    directly counteracting my last commit, the dev/blaze/dev file/namespace
    does work but it just needed the clojure tools lib. This is loaded with the
    clj "test" alias but that also directly rush a test runner and so cant be used
    for development. Its a mystery to me how this works as is, but one solution is
    to just create and use a dev namesapce that contains the missing lib.

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((clojure-mode
+
+  ;; select deps as the build tool
+  (cider-preferred-build-tool . clojure-cli)
+  ;; include dev dependencies when running
+  (cider-clojure-cli-global-options . "-A:dev")))
