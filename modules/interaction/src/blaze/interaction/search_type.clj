a8009c52fd4d19f60eaace482a7be654fda48708
benwiz <bwisialowski@gmail.com>
2019-11-27 11:12:47 -0800

    remove log

--- modules/interaction/src/blaze/interaction/search_type.clj
+++ modules/interaction/src/blaze/interaction/search_type.clj
@@ -78,9 +78,6 @@
 
 
 (defn- transform [mapping resource]
-  (println)
-  (clojure.pprint/pprint resource)
-  (println)
   (prewalk (fn [node]
              (if (vector? node)
                (let [[k v] node]
