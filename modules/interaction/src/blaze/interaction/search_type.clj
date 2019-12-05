4d4901cfc261a013195d6c292cff198c6c642e89
benwiz <bwisialowski@gmail.com>
2019-12-05 14:00:56 -0600

    re-enable full search_type fn

--- modules/interaction/src/blaze/interaction/search_type.clj
+++ modules/interaction/src/blaze/interaction/search_type.clj
@@ -103,8 +103,8 @@
                            false))))
              (map #(dissoc % :db/id))
              (take (fhir-util/page-size query-params))
-             #_(map #(transforms/transform db mapping %))
-             #_(map #(entry router %)))
+             (map #(transforms/transform db mapping %))
+             (map #(entry router %)))
             (d/datoms db :avet :phi.element/type (str "fhir-type/" type)))))
         clojure.pprint/pprint)))
 
