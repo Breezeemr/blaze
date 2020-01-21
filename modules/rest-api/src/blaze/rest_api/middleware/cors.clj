62e0eefc50f7cd8ea857dbb805282e796d9d5bc9
drew verlee <dverlee@breezeehr.com>
2020-01-21 16:27:34 -0600

    improve security limiting the cors domains

--- modules/rest-api/src/blaze/rest_api/middleware/cors.clj
+++ modules/rest-api/src/blaze/rest_api/middleware/cors.clj
@@ -5,11 +5,18 @@
 
 (defn wrap-cors
   [handler]
-  (fn [request]
-    (-> (handler request)
+  ;;TODO pull allowed origins from a config
+  ;;Have the configs depend on environment: e.g development should allow for all of these but produciton only the top two.
+  (let [allowed-origin? #{"https://localhost:8700"
+                          "http://localhost:8700"
+                          "https://storage.googleapis.com/portal.breezeehr.com"
+                          "https://portal.breezeehr.com"}]
+    (fn [{{origin "origin"} :headers
+         :as               request}]
+      (-> (handler request)
         (md/chain' #(-> %
-                      ;;TODO it might improve security to tighten which origins we allow requests from
-                      ;;Specifically, we could allow requests from only the patient portal
-                        (assoc-in [:headers "Access-Control-Allow-Origin"] "*")
-                        (assoc-in [:headers "Access-Control-Allow-Headers"] "Accept, Content-Type, Authorization")
-                        (assoc-in [:headers "Access-Control-Max-Age"] "3600"))))))
+                      ;;TODO in the case when the origin doesn't pass the whitelist its not clear (atm) how to convey that their were
+                      ;;multiple other origin options. So lets just send back one randomly for now and think about how to improve this.
+                      (assoc-in [:headers "Access-Control-Allow-Origin"] (or (allowed-origin? origin) (first allowed-origin?)))
+                      (assoc-in [:headers "Access-Control-Allow-Headers"] "Accept, Content-Type, Authorization")
+                      (assoc-in [:headers "Access-Control-Max-Age"] "3600")))))))
