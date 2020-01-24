b8888085bebae4d38285f3e0191ef91cc749ad55
drew verlee <dverlee@breezeehr.com>
2020-01-24 13:59:36 -0600

    return a 403 if the server rejects the origin.
    An alternative would be to return the request with an allowed origin.
    Either way, we need to short exit out of the middleware now and return
    as up steam handlers shouldn't handle it after the origin isn't allowed.
    
    TODO we want to accept google buckets

--- modules/rest-api/src/blaze/rest_api/middleware/cors.clj
+++ modules/rest-api/src/blaze/rest_api/middleware/cors.clj
@@ -1,25 +1,36 @@
 (ns blaze.rest-api.middleware.cors
   (:require
-    [manifold.deferred :as md]
-    [ring.util.response :as ring]))
+   [manifold.deferred :as md]))
 
-(defn wrap-cors
-  [handler]
-  ;;TODO pull allowed origins from a config
-  ;;Have the configs depend on environment: e.g development should allow for all of these but produciton only the top two.
-  (let [allowed-origin? #{"https://localhost:8700"
-                          "http://localhost:8700"
-                          "https://storage.googleapis.com/portal.breezeehr.com"
-                          "https://portal.breezeehr.com"}]
+(def allowed-origins
+  #{"https://localhost:8700"
+    "http://localhost:8700"
+    ;;TODO handle this: https://storage.googleapis.com/portal.breezeehr.com
+    "https://portal.breezeehr.com"})
+
+;;TODO this should be configurable
+;:TODO needs to handle more then just origins
+(defn allowed-request?
+  [{{origin "origin"} :headers}]
+  (allowed-origins origin))
+
+(defn get-wrap-cors
+  "Fills the traditional role of rejecting requests from sources we dont trust. Traditionally cors deals
+  with just origins. But in our case, we need to be more specific e.g https://storage.googleapis.com/portal.breezeehr.com
+  's domain isn't enough.
+
+  Takes a function which will return a falsy value if the request is denied.
+  "
+  [allowed-request?]
+  (fn [handler]
     (fn [{{origin "origin"} :headers
          :as               request}]
-      (-> (handler request)
-        (md/chain' #(-> %
-                      ;;TODO in the case when the origin doesn't pass the whitelist its not clear (atm) how to convey that their were
-                      ;;multiple other origin options. So lets just send back one randomly for now and think about how to improve this.
+      (if-not (allowed-request? request)
+        ;;NOTE There is no spec for a failed cors request response because traditionally its handled by
+        ;;the client. So here we just return 403.
+        {:status 403 :body "Unauthorized"}
+        (md/chain' (handler request) #(update % :headers into {"Access-Control-Allow-Origin"  origin
+                                                               "Access-Control-Allow-Methods" "GET, OPTIONS"
+                                                               "Access-Control-Allow-Headers" "Accept, Content-Type, Authorization"
+                                                               "Access-Control-Max-Age"       "3600"}))))))
 
-                      ;;TODO do something better then multiple assoc-in calls
-                      (assoc-in [:headers "Access-Control-Allow-Origin"] (or (allowed-origin? origin) (first allowed-origin?)))
-                      (assoc-in [:headers "Access-Control-Allow-Methods"] "GET, OPTIONS")
-                      (assoc-in [:headers "Access-Control-Allow-Headers"] "Accept, Content-Type, Authorization")
-                      (assoc-in [:headers "Access-Control-Max-Age"] "3600")))))))
